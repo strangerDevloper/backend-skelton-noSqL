cb98e2fb810d6387ad02630ba1caf6d8dc4a7b6b
tusharMrikal <tushar@mrikal.com>
2025-03-01 17:51:21 +0530

    middleware setup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import "dotenv/config"; // Load environment variables from .env
 import express from "express";
+import cors from "cors";
 
 import db from "./utils/db";
 
 const app = express();
 const port = process.env.PORT || 3000; // Use the PORT variable from .env
+
+app.use(express.json({ limit: '50mb' })); // Middleware to parse JSON bodies
+app.use(cors());
+app.options('*', cors());
 
 // Initialize database connection when the application starts
 db.getDb()
@@ -19,8 +24,11 @@ db.getDb()
     process.exit(1);
   });
 
-app.get("/", (req, res) => {
-  res.send("Hello, environment variables!");
+app.get("/healthcheck", (req, res) => {
+  res.status(200).json({ 
+    message: "Server is running" 
+  });
+  console.log("achdbchdbh")
 });
 
 // Graceful shutdown
